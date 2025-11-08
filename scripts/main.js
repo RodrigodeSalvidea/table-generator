13e01c32f284e29a426f043a09b0c076429c0b31
Rodrigo de Salvidea <rodesalvideal@gmail.com>
2025-11-08 13:25:39 -0500

    move stuff out of main

--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,8 @@ const submitButton = document.querySelector('#submit-grammar-button')
 
 submitButton.addEventListener('click', () => {
   const  rulesString = rulesArea.value
-  const  ruleStrings = rulesString.split(/\n+/)
-  const  rulesList = ruleStrings.map(rule => makeRuleObjects(rule)).filter(rule => rule)
-  rulesList.forEach(rule => {
-    console.log(`${rule.getLeftSide()} ::= ${rule.getRightSide()}`)
-  })
-  registerSymbols(rulesList)
-  const rulesMap = condenseRules(rulesList)
-  const firsts = computeFirst(rulesMap)
-  console.log(terminalSymbols)
-  console.log(nonTerminalSymbols)
-  console.log(symbols)
- 
-  
-
+  Rules.initializeRules(rulesString)
+   
 })
 
 
